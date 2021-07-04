eaa27e731644b4b4d7b7eec20bdfc42e96c9e40e
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-07-04 01:32:14 -0500

    Add game functional at console

---
+++ main.js
@@ -0,0 +1,51 @@
+function getComputerPick()
+{
+    var randomPick = Math.round(Math.random() * 2);
+    switch(randomPick)
+    {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+}
+function game(playerPick)
+{
+    var computerPick = getComputerPick();
+    console.log("Player pick is "+playerPick);
+    console.log("Computer pick is "+computerPick);
+    if(playerPick ==computerPick)
+    {
+        console.log("Tie! Roll again");
+    } 
+    else if (playerPick == "Rock" && computerPick == "Scissors")
+    {
+        console.log("Player wins! Rocks beats Scissors");
+    } 
+    else if (playerPick == "Paper" && computerPick == "Rock")
+    {
+        console.log("Player wins! Paper beats Rock");
+    } 
+    else if (playerPick == "Scissors" && computerPick == "Paper")
+    {
+        console.log("Player wins! Scissors beats Paper");
+    } 
+    else if (playerPick == "Scissors" && computerPick == "Rock")
+    {
+        console.log("Computer wins! Rocks beats Scissors");
+    } 
+    else if (playerPick == "Rock" && computerPick == "Paper")
+    {
+        console.log("Computer wins! Paper beats Rock");
+    } 
+    else if (playerPick == "Paper" && computerPick == "Scissors")
+    {
+        console.log("Computer wins! Scissors beats Paper");
+    } 
+
+}
